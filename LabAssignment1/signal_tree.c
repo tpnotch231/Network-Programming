afeed11ad05dee41945c82ef8d94c7d9a5da3323
Aditya <ramaswamy.aditya@gmail.com>
2020-02-04 15:04:54 +0530

    The tree can now be created properly. Leaf nodes are identified by both their children having value 0.

--- LabAssignment1/signal_tree.c
+++ LabAssignment1/signal_tree.c
@@ -1,17 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-void procCreate(int n)
+#include<unistd.h>
+#include<math.h>
+void swap(int *n1,int *n2)
 {
-	
-	if(n>0&&!fork())
+	int temp=*n1;
+	*n1=*n2;
+	*n2=temp;
+}
+void procCreate(int n,int *child1,int *child2)
+{
+	int n1, n2;
+	n2=log2(n);
+	n2=pow(2,n2-1)-1;
+	n1=n-n2;
+	if(n1>=2*n2+1)
 	{
-		procCreate((n-2)/2);
+		n1-=n2+1;
+		n2+=n2+1;
+		if(n1<n2)
+		swap(&n1,&n2);
+	}
+	if(n1>0&&!(--n1,(*child1=fork())))
+	{
+		procCreate(n1,child1,child2);
 	}
 	else
 	{
-		if(n>0&&!fork())
+		if(n2>0&&!(--n2,(*child2=fork())))
 		{
-			procCreate((n-2)/2);
+			*child1=0;
+			procCreate(n2,child1,child2);
 		}
 		else
 		{
@@ -21,6 +40,13 @@ void procCreate(int n)
 }
 int main(int argc, char *argv[])
 {
-	procCreate(atoi(argv[1]));
-	sleep(100);
+	printf("Topmost parent: %d\n", getpid());
+	int child1=0, child2=0;
+	procCreate(atoi(argv[1])-1,&child1,&child2);
+	//sleep(5);
+	if(child1||child2)
+	{
+		printf("%d -> %d\n",getpid(),child1);
+		printf("%d -> %d\n",getpid(),child2);
+	}
 }
